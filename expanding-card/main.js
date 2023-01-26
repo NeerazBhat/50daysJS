d492e2e40ccaab678aaf25b0ca9a9d6611470e7e
Niraj Bhat <niraj@fusemachines.com>
2023-01-26 20:43:31 +0545

    feat: click action added on image list

--- expanding-card/main.js
+++ expanding-card/main.js
@@ -1,1 +1,16 @@
 import "./style.scss";
+
+const imageUiList = document.querySelectorAll(".image-wrapper");
+
+function removeActiveClasses() {
+  imageUiList.forEach((image) => {
+    image.classList.remove("active");
+  });
+}
+
+imageUiList.forEach((image) => {
+  image.addEventListener("click", () => {
+    removeActiveClasses();
+    image.classList.add("active");
+  });
+});
